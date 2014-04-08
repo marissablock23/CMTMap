8ad2536f490c1e4048c2ef22350821e799c999a0
marissablock23 <marissablock23@gmail.com>
2014-04-08 10:14:43 -0400

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -62,8 +62,8 @@ var MapsLib = {
       styles: [
         {
           stylers: [
-            { saturation: -100 }, // MODIFY Saturation and Lightness if needed
-            { lightness: 40 }     // Current values make thematic polygon shading stand out over base map
+            { saturation: 100 }, // MODIFY Saturation and Lightness if needed
+            { lightness: 100 }     // Current values make thematic polygon shading stand out over base map
           ]
         }
       ]
