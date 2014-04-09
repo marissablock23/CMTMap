d183e6f2d1d9239dcc90dd01ea57e3def66cbc75
marissablock23 <marissablock23@gmail.com>
2014-04-08 22:03:35 -0400

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -85,7 +85,7 @@ var MapsLib = {
     MapsLib.polygon1 = new google.maps.FusionTablesLayer({
       query: {
         from:   MapsLib.polygon1TableID,
-        select: "geometry"
+        select: "Address"
       },
       styleId: 2,
       templateId: 2
