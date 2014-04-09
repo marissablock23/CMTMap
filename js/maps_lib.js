d2e38c64a745436b8d899f4966325907fa51261d
marissablock23 <marissablock23@gmail.com>
2014-04-08 23:15:17 -0400

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -126,20 +126,20 @@ var MapsLib = {
   //-----custom filters for point data layer
     //---MODIFY column header and values below to match your Google Fusion Table AND index.html
     //-- TEXTUAL OPTION to display legend and filter by non-numerical data in your table
- //var type_column = "'Program Type'";  // -- note use of single & double quotes for two-word column header
-   // var tempWhereClause = [];
-    //if ( $("#cbType1").is(':checked')) tempWhereClause.push("Interdistrict");
-    //if ( $("#cbType2").is(':checked')) tempWhereClause.push("District");
-    //if ( $("#cbType3").is(':checked')) tempWhereClause.push("MorePreK");
-    //whereClause += " AND " + type_column + " IN ('" + tempWhereClause.join("','") + "')";
+    var type_column = "Type";  // -- note use of single & double quotes for two-word column header
+    var tempWhereClause = [];
+    if ( $("#cbType1").is(':checked')) tempWhereClause.push("Charter");
+    if ( $("#cbType2").is(':checked')) tempWhereClause.push("District");
+    if ( $("#cbType3").is(':checked')) tempWhereClause.push("Magnet");
+    whereClause += " AND " + type_column + " IN ('" + tempWhereClause.join("','") + "')";
 
     //-- NUMERICAL OPTION - to display and filter a column of numerical data in your table, use this instead
-    var type_column = "Category";
-    var searchType = type_column + " IN (-1,";
-    if ( $("#cbType1").is(':checked')) searchType += "1,";
-    if ( $("#cbType2").is(':checked')) searchType += "2,";
-    if ( $("#cbType3").is(':checked')) searchType += "3,";
-    whereClause += " AND " + searchType.slice(0, searchType.length - 1) + ")";*/
+   // var type_column = "Category";
+    //var searchType = type_column + " IN (-1,";
+    //if ( $("#cbType1").is(':checked')) searchType += "1,";
+    //if ( $("#cbType2").is(':checked')) searchType += "2,";
+    //if ( $("#cbType3").is(':checked')) searchType += "3,";
+    //whereClause += " AND " + searchType.slice(0, searchType.length - 1) + ")";*/
     
     //-------end of custom filters--------
 
